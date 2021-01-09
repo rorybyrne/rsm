bd683ef0361ecefc9e0b22640bf87fb380a80929
synek <rory@rory.bio>
2021-01-09 15:50:55 +0000

    Implements register, and adds URL validation
    What does this commit do?
    =========================
    * Uses the UrlService to implement the register command
    * Adds URL validation to the URL model
    * Adds the blast.txt file

--- msr/commands/register.py
+++ msr/commands/register.py
@@ -4,11 +4,24 @@
 """
 import click
 
+from msr.model import URL
+from msr.service.url_service import UrlService
+
 
 @click.command()
-def register():
+@click.argument('url')
+def register(url: str):
     """Adds a new URL to the registry
 
-    ...
+    Params:
+        url     The URL to be registered.
     """
-    pass
+    url_service = UrlService()
+
+    try:
+        url = URL(url)
+        url_service.store_url(url)
+        print("URL stored.")
+    except ValueError as e:
+        print("Invalid URL.")
+        exit(1)

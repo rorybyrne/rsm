bfc919ecaf62b88fb67b3b4d467667c89c8b10ed
synek <rory@rory.bio>
2021-01-09 12:30:09 +0000

    Adds skeletons for the other commands
    What does this commit do?
    =========================
    * Creates empty commands for register, race, measure
    * Hooks them all up to the entrypoint via Click
    
    Why are we doing this?
    ======================
    * So that we can easily test the commands as we work on them
    
    Notes
    =====
    Closes #2

---
+++ msr/commands/measure.py
@@ -0,0 +1,14 @@
+"""Measure command
+
+@author Rory Byrne <rory@rory.bio>
+"""
+import click
+
+
+@click.command()
+def measure():
+    """Measures the body sizes of all URLs in the registry
+
+    ...
+    """
+    pass
---
+++ msr/commands/race.py
@@ -0,0 +1,14 @@
+"""Race command
+
+@author Rory Byrne <rory@rory.bio>
+"""
+import click
+
+
+@click.command()
+def race():
+    """Measures the average response times of domains in the URL registry
+
+    ...
+    """
+    pass

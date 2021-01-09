e3f3c47325d0fb6b3721814721519a1726bd4ed4
synek <rory@rory.bio>
2021-01-09 13:02:59 +0000

    Adds a UrlService to interface with the registry
    What does this commit do?
    =========================
    * Adds a msr/service directory with service classes
    * Adds a simple service for storing and fetching URLs
    
    Why are we doing this?
    ======================
    * So that we can handle business logic outside the CLI code
    
    Notes
    =====
    Closes #4

---
+++ msr/service/url_service.py
@@ -0,0 +1,47 @@
+"""Registry service to store URLs
+
+@author Rory Byrne <rory@rory.bio>
+"""
+from typing import List
+
+from msr.persist.exceptions import StorageAccessException
+from msr.persist.registry.FileRegistry import FileRegistry
+from msr.util.log import Logger
+
+
+class RegistryException(Exception):
+    """Failed to access the registry"""
+    pass
+
+
+class UrlService(Logger):
+    """Service exposing the registry backend to consumers
+
+    Note: This should probably be a data layer rather than a service, but we're being concise.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.registry = FileRegistry()  # Later we should inject this via dependency injection
+
+    def store_url(self, url: str) -> None:
+        """Store the given URL in the registry"""
+        try:
+            self.registry.save(url)
+        except StorageAccessException as e:
+            self.log.error(e)
+            raise RegistryException("Could not save URL in the registry.")
+        except Exception as e:
+            self.log.error(e)
+            raise RegistryException(e)
+
+    def fetch_urls(self) -> List[str]:
+        """Fetch all of the URLs from the registry"""
+        try:
+            return self.registry.get_all()
+        except StorageAccessException as e:
+            self.log.error(e)
+            raise RegistryException("Could not find URLs")
+        except Exception as e:
+            self.log.error(e)
+            raise RegistryException(e)

c319d0db5f5a502654d66b63d83328fb65e52f69
synek <rory@rory.bio>
2021-01-09 11:49:16 +0000

    Replaces pyproject.toml with a setup.py file
    What does this commit do?
    =========================
    * Deletes the pyproject.toml
    * Creates a setup.py configuring the CLI entrypoint
    
    Why are we doing this?
    ======================
    * pyproject.toml is not supported by pip install editable mode

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='msr',
+    version='0.1.0',
+    py_modules=['msr'],
+    install_requires=[],
+    entry_points='''
+        [console_scripts]
+        msr=msr.cli:msr
+    ''',
+)
